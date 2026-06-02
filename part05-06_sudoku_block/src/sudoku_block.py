fc29ad31b2ecf2d4d49513c1b6276f9ad24be046
rafael-areias <274752925+rafael-areias@users.noreply.github.com>
2026-06-02 14:48:45 +0100

    exercises 5.06 + 5.07

--- part05-06_sudoku_block/src/sudoku_block.py
+++ part05-06_sudoku_block/src/sudoku_block.py
@@ -1,1 +1,48 @@
 # Write your solution here
+def block_correct(sudoku: list, row_no: int, column_no: int):
+    check = []
+    new_matrix = []
+    for row in sudoku[row_no:row_no+3]:
+        new_row = row[column_no:column_no+3]
+        new_matrix.append(new_row)
+        
+    for i in new_matrix:
+        for j in i:
+            if j in check and j > 0:
+                return False
+            check.append(j)
+
+    return True
+        
+
+
+if __name__ == "__main__":
+    sudoku = [
+    [9, 0, 0, 0, 8, 0, 3, 0, 0],
+    [2, 0, 0, 2, 5, 0, 7, 0, 0],
+    [0, 2, 0, 3, 0, 0, 0, 0, 4],
+    [2, 9, 4, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 7, 3, 0, 5, 6, 0],
+    [7, 0, 5, 0, 6, 0, 4, 0, 0],
+    [0, 0, 7, 8, 0, 3, 9, 0, 0],
+    [0, 0, 1, 0, 0, 0, 0, 0, 3],
+    [3, 0, 0, 0, 0, 0, 0, 0, 2]
+    ]
+
+    print(block_correct(sudoku, 0, 0))
+    print(block_correct(sudoku, 1, 2))
+
+
+
+
+## solution
+
+def block_correct(sudoku: list, row_no: int, column_no: int):
+    numbers = []
+    for r in range(row_no, row_no+3):
+        for s in range(column_no, column_no+3):
+            number = sudoku[r][s]
+            if number > 0 and number in numbers:
+                return False
+            numbers.append(number)
+    return True
